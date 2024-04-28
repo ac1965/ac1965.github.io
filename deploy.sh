b788c249e64974a38dbfcfdceaa19c7432c3a2b2
YAMASHITA Takao <tjy1965@gmail.com>
2024-04-28 11:43:31 +0900

    Update

--- deploy.sh
+++ deploy.sh
@@ -19,23 +19,38 @@ warn ()
     echo -e "\033[1;30m>\033[0;33m>\033[1;33m> \033[0m${@}\n"
 }
 
+cover ()
+{
+    case ${m} in
+        01|05|09)
+            echo 01 05 09;;
+        02|06|10)
+            echo 02 06 10;;
+        03|07|11)
+            echo 03 07 11;;
+        04|08|12)
+            echo 04 08 12;;
+    esac
+}
+
 test -d ${hugo} && cd ${hugo} || abort "${hogo} directory not found."
 # clean public
 rm -fr public
 
+# set cover
+m=$(date "+%m")
 info "Put cover image post directory"
 cd ${hugo}/content/post && \
     for f in $(ls --color=none *.md)
     do
         d=$(basename $f .md)
         if [ -d ${d} ]; then
-            test -f ${d}/cover.* || cp ${hugo}/assets/img/cover.jpg ${d}
+            test -f ${d}/cover.* || cp ${hugo}/assets/img/cover/${m}.jpg ${d}
         else
             mkdir ${d}
-            cp ${hugo}/assets/img/cover.jpg ${d}
+            cp ${hugo}/assets/img/cover/${m}.jpg ${d}
         fi
     done
-
 
 info "Deploying updates to GitHub..."
 cd ${hugo}
